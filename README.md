6e50239d3ab51c288945f85a3947abda7f5ea038
Hossam Hassan <hossam.sahss120@gmail.com>
2024-01-20 23:10:32 +0200

    fix bugs in Create Table File and Modify README file

--- README.md
+++ README.md
@@ -1,3 +1,45 @@
+# Database Engine Bash Project
+
+This project Simulates Database Engine like SQL, POSTGRE, ...
+Wrote and interpreted with Bash
+
+## Table of Contents
+
+- [Database Engine Bash Project](#database-engine-bash-project)
+  - [Table of Contents](#table-of-contents)
+  - [About](#about)
+  - [Features](#features)
+  - [Getting Started](#getting-started)
+    - [Installation](#installation)
+  - [Contributors:](#contributors)
+  - [References:-](#references-)
+
+## About
+
+Wrote and interpreted with Bash 
+This project is a simple CRUD operations of Database Engine 
+like : create Databases and Tables into Database, list 
+databases, tabels, remove database, tables ...  
+
+## Features
+
+- **Data Storage:** Persistent storage of data in simple text files.
+- **CRUD Operations:** Basic support for Create, Read, Update, and Delete operations.
+- **Query Language:** Simple query language for retrieving data.
+
+## Getting Started
+
+Follow these instructions to get the project up and running on your local machine.
+
+
+### Installation
+
+Clone the repository and navigate to the project directory:
+
+```bash
+$ git clone https://github.com/HossamHassan70/Database_Engine.git
+$ cd Database_Engine
+$ ./main-menu.sh
 
 
 - main-menu.sh file
@@ -7,7 +49,11 @@
     * Connect to DB Handling
 - table-menu.sh file
     Direction to All another files of table operation
- 
-References:-
+
+## Contributors:
+* [Hossam Hassan](https://github.com/HossamHassan70)
+* [Amany Abdelrahman](https://github.com/amany-abdelrahman)
+
+## References:-
 1- https://gist.github.com/fnky/458719343aabd01cfb17a3a4f7296797
 2- https://en.wikipedia.org/wiki/ANSI_escape_code
--- create-table.sh
+++ create-table.sh
@@ -1,113 +1,102 @@
 #!/usr/bin/bash
 
-regex="^[a-zA-Z][a-zA-Z0-9_]*$"
+regex="^[a-zA-Z][a-zA-Z0-9_]{0,14}$"
+
 read -p "Enter name of a new table (or '0' to back): " name
 if [[ $name == "0" ]]; then
     clear
     cd ../../
     . table-menu.sh
 fi
-if [[ ${#name} -gt 1 ]]; then
+name=$(echo "$name" | sed 's/[^a-zA-Z0-9_ ]//g' | tr " " "_")
+if [[ ${#name} -gt 1 ]] && [[ ${#name} -le 16 ]]; then
     if [ -f "$name" ]; then
-        echo "Sorry, you entered an existing table"
+        echo -e "\e[91mSorry, you entered an existing table\e[0m"
+        cd ../../
         . table-menu.sh
-    elif [[ -z "$name" ]] || ! [[ "$name" =~ $regex ]]; then
-        echo "Sorry, you entered an invalid name."
+    elif [[ -z "$name" ]] || [[ ! "$name" =~ $regex ]]; then
+        echo -e "\e[91mSorry, you entered an invalid name\e[0m"
+        cd ../../
         . table-menu.sh
     else
         touch "$name"
-        echo "Table created successfuly"
     fi
 else
     echo -e "\e[91mError: Invalid Table Name (should be more than one character)\e[0m"
+    cd ../../
     . table-menu.sh
 fi
 clear
-while true 
-do  
-    echo -e "Enter the number of fields of \e[93m[$name]\e[0m: " 
+echo -e "\e[92mTable \e[93m[$name] \e[92mcreated successfuly\e[0m"
+PS3="$name > "
+
+while true; 
+do
+    echo -e "Enter The number of fields in \e[93m[$name]\e[0m table: "
     read num
     if [[ $num -eq 0 ]]; then
         echo "You can't enter $num as a number of fields."
-    elif [[ $num =~ ^[1-9][0-9]*$ ]]; then
+    elif [[ $num =~ ^([1-9]|1[0-9])$ ]]; then
         echo "Number of fields: $num"
         break
     else
-        echo "Invalid number. Please enter a valid one."
-    fi 
+        echo "Invalid number. Maximum Columns is '19' ."
+    fi
 done
-echo "Now you can enter metadata for your table :)"
-echo "Your primary key Must be the first column"
-rowtype=""
-pk_column=""
-declare -A column_names
-for ((i=1; i<=$num; i++))
-do  
-    column_names=()
-    while true
-    do 
+echo -e "\e[33mNow you can enter metadata for your table\e[0m"
+echo "Your primary key is 'int' & Must be the first column"
+echo -e "\e[34m--------------------------------------------------------\e[0m"
+# rowtype=""
+read -p "Name of column number 1 is (Recommended be ID): " one_name
+echo -n "$one_name" >>"$name"
+echo -n ":" >>"$name"
+
+for ((i = 2; i <= $num; i++)); 
+do
+    while true; 
+    do
         read -p "Name of column number $i is: " cname
-        # check if cname value in column_name or not
-        if [[ "${column_names[@]}" =~ "$cname" ]]; then
-            echo "This name has already been used. Try another one."
-            continue
-        fi
-        column_names+=("$cname") 
-        # while [[ -z "$cname" ]] || [[ -n "${column_names[$cname]}" ]]
-        # do
-        #     if [[ -n "${column_names[$cname]}" ]]; then
-        #         echo "This column name already exists. Please enter a different name."
-        #     else
-        #         echo "Name can't be empty, please enter valid name"
-        #     fi
-        #     # read cname
-        # done
-        column_names["$cname"]=1
-        if [[ $cname =~ ^[a-zA-Z_][a-zA-Z0-9_]*$ ]]; then
-            if [[ $i -eq 1 ]]; then
-                pk_column="$cname"
-            fi
-            rowtype+=" $cname"
+        if [[ $cname =~ $regex ]]; then
+            echo -n "$cname">>"$name"
+            # break
+            if [ $i -lt $num ];then
+                echo -n ":" >>"$name"
+            fi 
             break
         else
-            echo "Invalid name."
+            echo -e "\e[91mInvalid name. Try again \e[0m"
         fi
     done
-    select choice in integer string
-    do 
-        case $choice in 
-            integer )
-                rowtype+=":integer"
-                break
-                ;;
-            string )
-                echo "String"
-                rowtype+=":string"
-                break
-                ;;
-            * )
-                echo "You entered an invalid datatype. Only integer and string are valid."
-                ;;
-        esac 
-    done
 done
-echo "$rowtype" >> "$name"
-echo "Metadata of your table is: $rowtype"
-    echo "Do you want to make $pk_column as the primary key? (y/n): "
-    read pk_choice
-    case $pk_choice in
-        [yY]) 
-            echo "$pk_column:integer:unique(Not NULL)" >> "$name"
-            echo "Primary key added successfully"
-            ;;
-        [nN])
-            echo "Primary key does not added."
-            ;;
-        *)
-            echo -e "\e[91mInvalid choice. Please enter 'yes' or 'no'.\e[0m"
-            rm -r $name 
-            echo -e "\e[91mThe Table [$name] will be deleted, Try Again\e[0m"
-            . create-table.sh
-            ;;
-    esac
+echo "" >>"$name"
+echo -n "int" >>"$name"
+for ((i = 2; i <= $num; i++)); 
+do
+    echo -e "\e[34mType of column number $i is: \e[0m"  
+        select choice in int str; 
+        do
+            case $REPLY in
+            1)
+                echo -n ":int">>"$name"
+                # rowtype+=":integer"
+                break
+                ;;
+            2)
+                echo -n ":str">>"$name"
+                # rowtype+=":string"
+                break
+                ;;
+            *)
+                echo -e "\e[91mError,invalid datatype.. only '1' for int & '2' for str are valid\e[0m"
+                ;;
+            esac
+        done
+done
+# this line to make a new line in table file 
+echo "" >>"$name"
+echo -e "\e[94m------------------------------------------------------------\e[0m"
+echo -e "\e[92mTable Metadata added to \e[93m[$name] \e[92mSuccessfully\e[0m"
+echo -e "\e[94m------------------------------------------------------------\e[0m"
+
+cd ../../
 . table-menu.sh
